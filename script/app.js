6df8f50c7bcc29dc463313c7f89f512242bafaa6
Kass-S <kassiespacek101@gmail.com>
2024-11-13 15:37:08 -0800

    got elements by id

---
+++ script/app.js
@@ -0,0 +1,16 @@
+
+
+let btnChange1 = document.getElementById('btnChange1');
+let btnChange2 = document.getElementById('btnChange2');
+let btnChange3 = document.getElementById('btnChange3');
+let btnChange4 = document.getElementById('btnChange4');
+let btnChange5 = document.getElementById('btnChange5');
+let btnChange6 = document.getElementById('btnChange6');
+let btnChange7 = document.getElementById('btnChange7');
+let btnChange8 = document.getElementById('btnChange8');
+let btnChange9 = document.getElementById('btnChange9');
+let btnChange10 = document.getElementById('btnChange10');
+
+
+btnChange1.addEventListener('click', function(event) {  
+});
